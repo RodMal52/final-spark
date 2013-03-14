e021e8025ea989fb37d4abd850ac375420652a71
Rodrigo Maldonado <rodmal13@gmail.com>
2013-03-14 23:15:43 +0000

    Agregué la funcionalidad de Game over

---
+++  final-spark/FinalSpark/src/mx/itesm/finalspark/GameOverActivity.java
@@ -0,0 +1,27 @@
+package mx.itesm.finalspark;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class GameOverActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game_over);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_game_over, menu);
+		return true;
+	}
+	public void mostrarMenu (View view){
+		Intent intent = new Intent(this,MenuActivity.class);
+		this.startActivity(intent);
+	}
+}
